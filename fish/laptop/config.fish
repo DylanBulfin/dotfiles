62ff91be06ce12f0078e4212ebc180aa263397d9
Dylan Bulfin <dylanbulfin@gmail.com>
2024-03-19 17:47:45 -0400

    Adding zsh, updating fish, alacritty

---
+++ fish/laptop/config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+    alias ls="ls --color=auto"
+    abbr -a -g ll ls -la
+    abbr -a -g ref source /home/dylan/.config/fish/config.fish
+    abbr -a -g e exit
+
+    fish_add_path /home/dylan/go/bin
+    fish_add_path /home/dylan/bin
+    fish_add_path "/home/dylan/.cargo/bin"
+    fish_add_path "/home/dylan/.cabal/bin"
+    fish_add_path "/home/dylan/.ghcup/bin"
+
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    fish_vi_key_bindings
+
+    set -U fish_greeting ""
+
+    bind --mode insert \cF forward-char
+
+    #Should stay at end
+    zoxide init fish | source
+end
