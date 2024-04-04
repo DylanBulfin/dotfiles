5261906ea667f9c70186e081731d3fd5a553ce97
Dylan Bulfin <dylanbulfin@gmail.com>
2024-04-04 15:57:06 -0400

    Adding more misc. windows updates

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -15,8 +15,17 @@ return {
       },
     },
     config = function()
+      local has_words_before = function()
+        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+          return false
+        end
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+      end
+
       local luasnip = require("luasnip")
       local cmp = require("cmp")
+
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -41,15 +50,17 @@ return {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true, -- Autoselect top option
           }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
+          ["<C-f>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true, -- Autoselect top option
+          }),
+          ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
             else
               fallback()
             end
-          end, { "i", "s" }),
+          end),
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
