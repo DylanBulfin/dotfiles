38d700bb56937f22f55a946b41c5a14cc4bd0ae0
Dylan Bulfin <dylanbulfin@gmail.com>
2024-05-10 21:23:21 -0400

    Splitting nvim config

---
+++ nvim_stable/lua/plugins/cmp.lua
@@ -0,0 +1,94 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "hrsh7th/cmp-nvim-lsp",
+      },
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+      },
+      {
+        "saadparwaiz1/cmp_luasnip",
+      },
+    },
+    config = function()
+      local has_words_before = function()
+        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+          return false
+        end
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = vim.tbl_deep_extend("force", cmp.config.window.bordered(), {
+            zindex = 50,
+          }),
+
+          documentation = vim.tbl_deep_extend("force", cmp.config.window.bordered(), {
+            zindex = 25,
+          }),
+        },
+        performance = { max_view_entries = 10 },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
+          ["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
+          -- C-b (back) C-f (forward) for snippet placeholder navigation.
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            -- select = true,
+            -- Uncomment above to auto-select top option with enter
+          }),
+          ["<C-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true, -- Autoselect top option
+          }),
+          ["<C-g>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true, -- Autoselect top option
+          }),
+          ["<C-h>"] = cmp.mapping(function()
+            if cmp.visible_docs() then
+              cmp.close_docs()
+            else
+              cmp.open_docs()
+            end
+          end),
+          ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
+            end
+          end),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        },
+      })
+    end,
+  },
+}
---
+++ nvim_stable/lua/plugins/copilot.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    lazy = true,
+    opts = {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        auto_trigger = true,
+        debouce = 100,
+        keymap = {
+          accept = "<C-f>",
+          next = "<C-n>",
+          prev = "<C-p>",
+        },
+      },
+      server_opts_override = {
+        settings = {
+          advanced = {
+            inlineSuggestCount = 3,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.keymap.set("n", "<leader>cs", "<Cmd>Copilot status<CR>", { desc = "Copilot status" })
+      vim.keymap.set("n", "<leader>ce", "<Cmd>Copilot enable<CR>", { desc = "Copilot enable" })
+      vim.keymap.set("n", "<leader>cd", "<Cmd>Copilot disable<CR>", { desc = "Copilot disable" })
+      require("copilot").setup(opts)
+    end,
+  },
+}
