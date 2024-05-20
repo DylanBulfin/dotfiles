9fe890f5375171c6152b7fb584b086b0354ee9f7
Dylan Bulfin <dylanbulfin@gmail.com>
2024-05-20 14:30:44 -0400

    Adding vscode keybinding utility

---
+++ vscode/generate.py
@@ -0,0 +1,50 @@
+import csv
+
+vscode_format = '    "key": "{}",\n    "command": "{}",\n    "when": "!editorFocus && !inputFocus"\n'
+nvim_format = 'vim.keymap.set({}, "{}", call("{}"))'
+
+def gen_vscode_binding(is_space, key, command):
+    if is_space:
+        return '  {\n' + vscode_format.format('space ' + key, command) + '  },'
+    else:
+        return '  {\n' + vscode_format.format(key, command) + '  },'
+
+def gen_vscode_config():
+    with open('mappings.csv', newline='') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            if len(line) != 4:
+                continue
+            is_space = line[1]
+            keys = line[2].replace('"', '').strip()
+            command = line[3].replace('"', '').strip()
+
+            print (gen_vscode_binding(is_space, keys, command))
+
+def nvim_key_format(key):
+    keys = [x.replace("space", "<leader>") for x in key.split(' ')]
+    return ''.join(keys)
+
+def gen_nvim_binding(mode, is_space, key, command):
+    if is_space:
+        return nvim_format.format(mode, '<space>' + nvim_key_format(key), command)
+    else:
+        return nvim_format.format(mode, nvim_key_format(key), command)
+
+def gen_nvim_config():
+    with open('mappings.csv', newline='') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            if len(line) != 4:
+                continue
+            is_space = line[1]
+            keys = line[2].replace('"', '').strip()
+            command = line[3].replace('"', '').strip()
+
+            if 'n' in line[0]:
+                print (gen_nvim_binding('n', is_space, keys, command))
+            if 'v' in line[0]:
+                print (gen_nvim_binding('v', is_space, keys, command))
+
+# gen_vscode_config()
+gen_nvim_config()
