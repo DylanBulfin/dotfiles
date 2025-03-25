c102876b6e7e95a491fa82c87579be2b39f0b841
Dylan Bulfin <dylanbulfin@gmail.com>
2025-03-25 17:08:36 -0400

    Add barbar, sessions

--- nvim/lua/plugins/qol.lua
+++ nvim/lua/plugins/qol.lua
@@ -10,4 +10,26 @@ return {
       vim.keymap.set({ "n", "x", "o" }, "st", "<Cmd>HopChar1<CR>")
     end,
   },
+  {
+    "echasnovski/mini.sessions",
+    config = function()
+      require("mini.sessions").setup({
+        directory = "",
+        file = ".session",
+        autoread = true,
+        autowrite = true,
+        -- hooks = {
+        --   pre = {
+        --     write = function()
+        --       vim.cmd("<Cmd>Neotree close<CR>")
+        --       if require("trouble").is_open("diagnostics") then
+        --         vim.cmd("<Cmd>Trouble diagnostics close<CR>")
+        --       end
+        --       vim.cmd("<Cmd>mksession .session<CR>")
+        --     end,
+        --   },
+        -- },
+      })
+    end,
+  },
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -8,15 +8,54 @@ return {
       vim.cmd.colorscheme("catppuccin-mocha")
     end,
   },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   dependencies = { "nvim-tree/nvim-web-devicons" },
+  --   config = function()
+  --     require("lualine").setup({
+  --       options = { theme = "catppuccin", globalstatus = true },
+  --     })
+  --   end,
+  -- },
   {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    "romgrk/barbar.nvim",
+    dependencies = {
+      "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+      "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+    },
     config = function()
-      require("lualine").setup({
-        options = { theme = "catppuccin", globalstatus = true },
+      vim.g.barbar_auto_setup = false
+
+      require("barbar").setup({
+        hide = { extensions = false },
+        letters = "arstneiocdhxzgmvkbjplfuwyq;ARSTNEIOCDHXZGMVKBJPLFUWYQ",
       })
+
+      -- Move to previous/next
+      vim.keymap.set("n", "<leader>N", "<Cmd>BufferPrevious<CR>", { desc = "Previous Buffer" })
+      vim.keymap.set("n", "<leader>n", "<Cmd>BufferNext<CR>", { desc = "Next Buffer" })
+      -- Goto buffer in position...
+      vim.keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", { desc = "Goto Buffer 1" })
+      vim.keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", { desc = "Goto Buffer 2" })
+      vim.keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", { desc = "Goto Buffer 3" })
+      vim.keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", { desc = "Goto Buffer 4" })
+      vim.keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", { desc = "Goto Buffer 5" })
+      vim.keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", { desc = "Goto Buffer 6" })
+      vim.keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", { desc = "Goto Buffer 7" })
+      vim.keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", { desc = "Goto Buffer 8" })
+      vim.keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", { desc = "Goto Buffer 9" })
+      vim.keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>", { desc = "Goto Last Buffer" })
+      -- Pin/unpin buffer
+      vim.keymap.set("n", "<leader>bP", "<Cmd>BufferPin<CR>", { desc = "Pin Buffer" })
+      -- Pick Buffer
+      vim.keymap.set("n", "<leader>bp", "<Cmd>BufferPick<CR>", { desc = "Buffer Pick Mode" })
+      -- Buffer Management
+      vim.keymap.set("n", "<leader>bc", "<Cmd>BufferClose<CR>", { desc = "Close Buffer" })
+      vim.keymap.set("n", "<leader>ba", "<Cmd>BufferCloseAllButPinned<CR>", { desc = "Close Buffer" })
+      vim.keymap.set("n", "<leader>bo", "<Cmd>BufferCloseAllButCurrentOrPinned<CR>", { desc = "Close Buffer" })
     end,
   },
+
   { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons", opts = {} },
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
   {
